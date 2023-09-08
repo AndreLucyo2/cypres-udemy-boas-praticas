fc1f960801fece08f3b5ded2b9ffcad7ee60379d
AndreLucyo2 <andre.lucyo2@gmail.com>
2023-09-08 15:19:11 -0300

    Execicio 01.1 - hardcoded-assertion

--- cypress/e2e/hardcoded-assertion/sample.cy.js
+++ cypress/e2e/hardcoded-assertion/sample.cy.js
@@ -10,7 +10,7 @@ describe('Hardcoded assertion bad practice', () => {
     cy.wait('@getStories')
   })
 
-  it('searches', () => {
+  it('searches alternativa 1', () => {
     //Se adapta a cada alteração nas fixtures
     cy.fixture('stories').then(({ hits }) => {
       cy.search('cypress.io');
@@ -19,7 +19,7 @@ describe('Hardcoded assertion bad practice', () => {
         .as('tableRows')
         .should('have.length', hits.length);
 
-      //valida coluna e valor: 
+      //valida coluna e valor: fica dinamico e o teste se adapta 
       hits.forEach((hit, index) => {
         cy.get('@tableRows')
           .eq(index)
@@ -32,4 +32,28 @@ describe('Hardcoded assertion bad practice', () => {
     });
 
   })
+
+  //ja inicia com a fixture carregada evita um .then
+  const { hits } = require('../../fixtures/stories');
+
+  it('searches alternativa 2', () => {
+
+    cy.search('cypress.io');
+    cy.wait('@getStories');
+
+    cy.get('.table-row')
+      .as('tableRows')
+      .should('have.length', hits.length);
+
+    //valida coluna e valor: fica dinamico e o teste se adapta 
+    hits.forEach((hit, index) => {
+      cy.get('@tableRows')
+        .eq(index)
+        .should('contain', hit.title)
+        .should('contain', hit.author)
+        .should('contain', hit.num_comments)
+        .should('contain', hit.points)
+    });
+  })
+
 })
