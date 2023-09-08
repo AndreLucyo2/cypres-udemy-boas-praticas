fd69ad4b96bd7cba0e94e5dd84967c8dcc107781
AndreLucyo2 <andre.lucyo2@gmail.com>
2023-09-08 13:22:46 -0300

    exercicio 05 - lodash

--- cypress/e2e/duplication/sample3.cy.js
+++ cypress/e2e/duplication/sample3.cy.js
@@ -9,23 +9,36 @@ describe('Code duplication bad practice - repetitive actions and assertions', ()
     cy.wait('@getStories')
   })
 
-  it('searches for the same term 3 times', () => {
-    cy.search('cypress.io')
+    //busca pelo mesmo termo 3 vezes usando o lodash :  https://lodash.com/
+    it('searches for the same term 3 times', () => {
 
-    cy.get('.table-row')
-      .its('length')
-      .should('be.at.least', 1)
+      Cypress._.times(3,()=>{
+        cy.searchCmd('cypress.io')
+        cy.get('.table-row')
+          .its('length')
+          .should('be.at.least', 1);
+      });
+    })
 
-    cy.search('cypress.io')
+  // //busca pelo mesmo termo 3 vezes
+  // it('searches for the same term 3 times', () => {
+  //   cy.search('cypress.io')
 
-    cy.get('.table-row')
-      .its('length')
-      .should('be.at.least', 1)
+  //   cy.get('.table-row')
+  //     .its('length')
+  //     .should('be.at.least', 1)
 
-    cy.search('cypress.io')
+  //   cy.search('cypress.io')
 
-    cy.get('.table-row')
-      .its('length')
-      .should('be.at.least', 1)
-  })
+  //   cy.get('.table-row')
+  //     .its('length')
+  //     .should('be.at.least', 1)
+
+  //   cy.search('cypress.io')
+
+  //   cy.get('.table-row')
+  //     .its('length')
+  //     .should('be.at.least', 1)
+  // })
+  
 })
