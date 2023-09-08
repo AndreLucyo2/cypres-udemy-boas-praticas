6b777af6039132efb7eb96e2135ba79a4fb3a429
AndreLucyo2 <andre.lucyo2@gmail.com>
2023-09-08 13:28:24 -0300

    exercicio 06

--- cypress/e2e/duplication/sample4.cy.js
+++ cypress/e2e/duplication/sample4.cy.js
@@ -3,9 +3,14 @@ describe('Code duplication bad practice - multiple checks', () => {
     cy.visit('https://bit.ly/2XSuwCW')
   })
  
-  it('checks all checkboxes from a specific fieldset', () => {
-    cy.get('#friend').check()
-    cy.get('#publication').check()
-    cy.get('#social-media').check()
+  it('checks all checkboxes from a specific element fieldset', () => {
+    cy.get('fieldset div input[type="checkbox"]').check();//marca todos os checks retornados no seletor
   })
+ 
+  // it('checks all checkboxes from a specific fieldset', () => {
+  //   cy.get('#friend').check()
+  //   cy.get('#publication').check()
+  //   cy.get('#social-media').check()
+  // })
+  
 })
