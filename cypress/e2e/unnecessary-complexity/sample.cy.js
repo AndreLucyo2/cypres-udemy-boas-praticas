8dc5352ab3dd28586904a1b7139658175b26d0c4
AndreLucyo2 <andre.lucyo2@gmail.com>
2023-09-08 15:29:55 -0300

    page-objects

--- cypress/e2e/unnecessary-complexity/sample.cy.js
+++ cypress/e2e/unnecessary-complexity/sample.cy.js
@@ -1,23 +1,25 @@
 describe('Unnecessary complexity bad practice', () => {
   beforeEach(() => {
     cy.visit('https://bit.ly/2XSuwCW')
-    cy.randomlyTogglePurchaseAgreement()
+    cy.randomlyTogglePurchaseAgreement() //marca aletariamente um chekbox
   })
 
   Cypress._.times(5, () => {
     it('checks the checkbox only if not checked', () => {
-      cy.get('body', { log: false }).then($body => {
-        if ($body.find('#agree:checked').length) {
-          cy.log('check box was checked')
-          return
-        }
-        cy.log('check box was unchecked')
-        $body.find('#agree').click()
-        return
-      })
+      // cy.get('body', { log: false }).then($body => {
+      //   if ($body.find('#agree:checked').length) {
+      //     cy.log('check box was checked')
+      //     return
+      //   }
+      //   cy.log('check box was unchecked')
+      //   $body.find('#agree').click()
+      //   return
+      // })
 
-      cy.get('#agree', { log: false })
+      cy.get('#agree')
+        .check()
         .should('be.checked')
+        
     })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -29,4 +29,18 @@ Cypress.Commands.add('search', term => {
         .should('be.visible')
         .clear()
         .type(`${term}{enter}`)
-});
+})
+
+Cypress.Commands.add('assertResults', () => {
+    cy.get('.table-row').then(rows => {
+        expect(rows.length).to.be.at.least(1)
+    })
+})
+
+Cypress.Commands.add('randomlyTogglePurchaseAgreement', () => {
+    if (Math.random() > 0.5) {
+        cy.get('#agree')
+            .click()
+    }
+})
+
